ae42fccac9b1aa4619df4f4f9499427d0a816887
Capozi <capozi.devv@gmail.com>
2026-02-14 00:13:41 -0500

    Update PlayerEntityMixin.java

--- src/main/java/net/capozi/demise/mixin/PlayerEntityMixin.java
+++ src/main/java/net/capozi/demise/mixin/PlayerEntityMixin.java
@@ -2,6 +2,7 @@ package net.capozi.demise.mixin;
 
 import net.capozi.demise.common.GameruleRegistry;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerEntityMixin {
     @Inject(method = "dropInventory", at = @At("HEAD"), cancellable = true)
     public void demise$dropInventory(CallbackInfo ci) {
+        if (((PlayerEntity)(Object)this).getWorld().getGameRules().getBoolean(GameRules.KEEP_INVENTORY)) return;
         if(((PlayerEntity)(Object)this).getWorld().getGameRules().getBoolean(GameruleRegistry.CREATE_GRAVE)) ci.cancel();
     }
 }
